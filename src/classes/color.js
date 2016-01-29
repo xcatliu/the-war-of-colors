292fd842cfddb141937b9f2427d85704f2c9f05a
xcatliu <xcatliu@gmail.com>
2016-01-29 19:52:32 +0800

    Add Color prototype

--- src/classes/color.js
+++ src/classes/color.js
@@ -1,8 +1,36 @@
-import ColorJS from 'color-js';
+import ColorClass from 'color';
 
-class Color extends ColorJS {
+const TYPES = {
+  ATTACK: 'ATTACK',
+};
+
+const skillsMap = {
+  36: {
+    name: 'Lightning',
+    type: TYPES.ATTACK,
+    attack: 38,
+    manacost: 30,
+  },
+};
+
+class Color extends ColorClass {
   constructor() {
     super(...arguments);
+  }
+  attack() {
+    return this.lightness();
+  }
+  defense() {
+    return 100 - this.saturation();
+  }
+  magicResistance() {
+    return this.saturation();
+  }
+  skills() {
+    const hue = this.hue();
+    return {
+      hue: skillsMap[hue],
+    };
   }
 }
 
--- test/color.js
+++ test/color.js
@@ -1,10 +1,32 @@
 import assert from 'assert';
 
 import Color from '../src/classes/Color';
+import ColorClass from 'color';
 
 describe('Color', () => {
+  const color = new Color('#FB5');
   it('should be instantiated successfully', () => {
-    const Green = new Color('#00FF00');
-    assert.equal(Green.getGreen(), 1);
+    assert.deepEqual(color instanceof ColorClass, true);
+  });
+  it('should has method red() green() blue()', () => {
+    assert.equal(color.red(), 255);
+    assert.equal(color.green(), 187);
+    assert.equal(color.blue(), 85);
+  });
+  it('should has method hue() saturation() lightness()', () => {
+    assert.equal(color.hue(), 36);
+    assert.equal(color.saturation(), 100);
+    assert.equal(color.lightness(), 67);
+  });
+  it('should has method attack() defense() magicResistance()', () => {
+    assert.equal(color.attack(), 67);
+    assert.equal(color.defense(), 0);
+    assert.equal(color.magicResistance(), 100);
+  });
+  it('should has method skills()', () => {
+    assert.deepEqual(
+      color.skills(),
+      { hue: { name: 'Lightning', type: 'ATTACK', attack: 38, manacost: 30 } }
+    );
   });
 });
